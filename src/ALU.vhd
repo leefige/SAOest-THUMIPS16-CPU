5107fcc8c4fd015e6cf789737978cb1972440579
乔逸凡 <qiaoyf15@mails.tsinghua.edu.cn>
2017-11-24 02:08:55 +0800

    Implement Controller and refine ALU.

--- src/ALU.vhd
+++ src/ALU.vhd
@@ -37,79 +37,50 @@ entity ALU is
         InputB   : in std_logic_vector (15 downto 0);
         ALUOp    : in std_logic_vector (3 downto 0);
 
-        ALUFlag  : out std_logic_vector (3 downto 0);
+        ALUFlag  : out std_logic_vector (1 downto 0);
         ALURes   : out std_logic_vector (15 downto 0)
     );
 end ALU;
 
 architecture Behavioral of ALU is
 
-    signal outputRes : std_logic_vector (16 downto 0);
+    signal tmpInputA : std_logic_vector (16 downto 0);
+    signal tmpInputB : std_logic_vector (16 downto 0);
+    signal result : std_logic_vector (16 downto 0);
 
 begin
 
-    ALURes <= outputRes;
+    tmpInputA <= InputA(15) & InputA;
+    tmpInputB <= InputB(15) & InputB;
 
-    process(ALUOp, InputA, InputB)
-        variable tmpInputA : std_logic_vector (16 downto 0);
-        variable tmpInputB : std_logic_vector (16 downto 0);
-        variable result : std_logic_vector (16 downto 0);
-    begin
-        tmpInputA := InputA(15) & InputA;
-        tmpInputB := InputB(15) & InputB;
-        case ALUOp is
-            when "0000" =>  -- Z
-                result := (others => 'Z');
-            when "0001" =>  -- ADD
-                result := tmpInputA + tmpInputB;
-            when "0010" =>  -- SUB
-                result := tmpInputA - tmpInputB;
-            when "0011" =>  -- AND
-                result := tmpInputA and tmpInputB;
-            when "0100" =>  -- OR
-                result := tmpInputA or tmpInputB;
-            when "0101" =>  -- SLL
-                result := to_stdlogicvector(to_bitvector(tmpInputA) sll conv_integer(InputB));
-            when "0110" =>  -- SRL
-                result := '0' & to_stdlogicvector(to_bitvector(InputA) srl conv_integer(InputB));
-            when "0111" =>  -- SRA
-                result := to_stdlogicvector(to_bitvector(tmpInputA) sra conv_integer(InputB));
-            when others =>
-                result := (others => '0');
-        end case;
-        outputRes <= result;
-        ALURes <= result(15 downto 0);
-    end process;
+    if ALUOp = "0000" then  -- Z
+        result <= (others => 'Z');
+    elsif ALUOp = "0001" then  -- ADD
+        result <= tmpInputA + tmpInputB;
+    elsif ALUOp = "0010" then  -- SUB
+        result <= tmpInputA - tmpInputB;
+    elsif ALUOp = "0011" then  -- AND
+        result <= tmpInputA and tmpInputB;
+    elsif ALUOp = "0100" then  -- OR
+        result <= tmpInputA or tmpInputB;
+    elsif ALUOp = "0101" then  -- SLL
+        result <= to_stdlogicvector(to_bitvector(tmpInputA) sll conv_integer(InputB));
+    elsif ALUOp = "0110" then  -- SRL
+        result <= '0' & to_stdlogicvector(to_bitvector(InputA) srl conv_integer(InputB));
+    elsif ALUOp = "0111" then  -- SRA
+        result <= to_stdlogicvector(to_bitvector(tmpInputA) sra conv_integer(InputB));
+    else
+        result <= (others => '0');
+    end if;
 
-    process(outputRes, ALUOp)
-        variable output : std_logic_vector (15 downto 0);
-    begin
-        output := outputRes(15 downto 0);
-        if (output = "0000000000000000") then
-            ALUFlag(0) <= '1';
-        else
-            ALUFlag(0) <= '0';
-        end if;
-        ALUFlag(1) <= outputRes(15);
+    ALURes <= result(15 downto 0);
 
-        case ALUOp is
-            when "0001" =>  -- ADD
-                ALUFlag(3) <=
-                    ((not InputA(15)) and (not InputB(15)) and outputRes(15)) or  -- a > 0, b > 0, a + b < 0
-                    (InputA(15) and InputB(15) and (not outputRes(15)));          -- a < 0, b < 0, a + b >= 0
-                if (conv_integer(output) < conv_integer(InputA)) then
-                    ALUFlag(2) <= '1';
-                else
-                    ALUFlag(2) <= '0';
-                end if;
-            when "0010" =>  -- SUB
-                ALUFlag(3) <=
-                    ((not InputA(15)) and InputB(15) and outputRes(15)) or        -- a > 0, b < 0, a - b < 0
-                    (InputA(15) and (not InputB(15)) and (not outputRes(15)));    -- a < 0, b > 0, a - b >= 0
-                ALUFlag(2) <= outputRes(16);
-            when others =>
-                ALUFlag(2) <= '0';
-                ALUFlag(3) <= '0';
-        end case;
-    end process;
+    if (result = "00000000000000000") then
+        ALUFlag(0) <= '1';
+    else
+        ALUFlag(0) <= '0';
+    end if;
+
+    ALUFlag(1) <= result(16);
+
 end Behavioral;
--- src/Controller.vhd
+++ src/Controller.vhd
@@ -55,6 +55,8 @@ architecture Behavioral of Controller is
     signal last8 : std_logic_vector (7 downto 0);
     signal last5 : std_logic_vector (4 downto 0);
     signal last2 : std_logic_vector (1 downto 0);
+    signal rx : std_logic_vector (2 downto 0);
+    signal ry : std_logic_vector (2 downto 0);
 
 begin
 
@@ -63,56 +65,386 @@ begin
     last2 <= Inst(1 downto 0);
     last5 <= Inst(4 downto 0);
     last8 <= Inst(7 downto 0);
+    rx <= Inst(10 downto 8);
+    ry <= Inst(7 downto 5);
 
     -------------------------------------------------------------------
     -- Controll Outputs For Each Instruction
     -------------------------------------------------------------------
     -- 算术
-    if    (first5 = "01001") then                           -- ADDIU
+    if (first5 = "01001") then                           -- ADDIU
+        RegWrEn <= '1';
+        MemWr <= '0';
+        MemRd <= '0';
+        WBSrc <= '1';
+        ALUop <= "0001"; -- plus
+        RegSrcA <= '0' & Inst(10 downto 8);
+        RegDst <= '0' & Inst(10 downto 8);
+        ExRes <= "11"; -- 3 stands for ExData get data from ALURes
+        ALUSrc <= '1'; -- imme
+
     elsif (first5 = "01000") then                           -- ADDIU3
+        RegWrEn <= '1';
+        MemWr <= '0';
+        MemRd <= '0';
+        WBSrc <= '1';
+        ALUOp <= "0001";
+        RegSrcA <= '0' & Inst(10 downto 8);
+        RegDst <= '0' & Inst(7 downto 5);
+        ExRes <= "11";
+        ALUSrc <= '1';
+
     elsif (first8 = "01100011") then                        -- ADDSP
+        RegWrEn <= '1';
+        MemWr <= '0';
+        MemRd <= '0';
+        WBSrc <= '1';
+        ALUOp <= "0001";
+        RegSrcA <= "1010";
+        RegDst <= "1010";
+        ExRes <= "11";
+        ALUSrc <= '1';
+
     elsif (first5 = "11100" and last2 = "01") then          -- ADDU
+        RegWrEn <= '1';
+        MemWr <= '0';
+        MemRd <= '0';
+        WBSrc <= '1';
+        ALUOp <= "0001";
+        RegSrcA <= '0' & Inst(10 downto 8);
+        RegSrcB <= '0' & Inst(7 downto 5);
+        RegDst <= '0' & Inst(4 downto 2);
+        ExRes <= "11";
+        ALUSrc <= '0'; -- regB
+
     elsif (first5 = "11100" and last2 = "11") then          -- SUBU
+        RegWrEn <= '1';
+        MemWr <= '0';
+        MemRd <= '0';
+        WBSrc <= '1';
+        ALUOp <= "0010";
+        RegSrcA <= '0' & Inst(10 downto 8);
+        RegSrcB <= '0' & Inst(7 downto 5);
+        RegDst <= '0' & Inst(4 downto 2);
+        ExRes <= "11";
+        ALUSrc <= '0'; -- regB
+
     elsif (first5 = "11101" and last5 = "01011") then       -- NEG
+        RegWrEn <= '1';
+        MemWr <= '0';
+        MemRd <= '0';
+        WBSrc <= '1';
+        ALUOp <= "0010";
+        RegSrcA <= "1100"; -- zero
+        RegSrcB <= '0' & Inst(7 downto 5);
+        RegDst <= '0' & Inst(10 downto 8);
+        ExRes <= "11";
+        ALUSrc <= '0'; -- regB
+
 
     -- 逻辑
     elsif (first5 = "11101" and last2 = "01100") then       -- AND
+        TRegType  <= 'Z';
+        RegWrEn   <= '1';
+        MemWr     <= '0';
+        MemRd     <= '0';
+        WBSrc     <= '1';
+        JumpType  <= (others => 'Z');
+        ALUOp     <= "0011";
+        RegSrcA   <= '0' & rx;
+        RegSrcB   <= '0' & ry;
+        RegDst    <= '0' & rx;
+        ExRes     <= "011";
+        ALUSrc    <= '0';
     elsif (first5 = "11101" and last5 = "01101") then       -- OR
+        TRegType  <= 'Z';
+        RegWrEn   <= '1';
+        MemWr     <= '0';
+        MemRd     <= '0';
+        WBSrc     <= '1';
+        JumpType  <= (others => 'Z');
+        ALUOp     <= "0100";
+        RegSrcA   <= '0' & rx;
+        RegSrcB   <= '0' & ry;
+        RegDst    <= '0' & rx;
+        ExRes     <= "011";
+        ALUSrc    <= '0';
 
     -- 移位
     elsif (first5 = "00110" and last2 = "00") then          -- SLL
+        TRegType  <= 'Z';
+        RegWrEn   <= '1';
+        MemWr     <= '0';
+        MemRd     <= '0';
+        WBSrc     <= '1';
+        JumpType  <= (others => 'Z');
+        ALUOp     <= "0101";
+        RegSrcA   <= '0' & ry;
+        RegSrcB   <= (others => 'Z');
+        RegDst    <= '0' & rx;
+        ExRes     <= "011";
+        ALUSrc    <= '1';
     elsif (first5 = "00110" and last2 = "11") then          -- SRA
+        TRegType  <= 'Z';
+        RegWrEn   <= '1';
+        MemWr     <= '0';
+        MemRd     <= '0';
+        WBSrc     <= '1';
+        JumpType  <= (others => 'Z');
+        ALUOp     <= "0111";
+        RegSrcA   <= '0' & ry;
+        RegSrcB   <= (others => 'Z');
+        RegDst    <= '0' & rx;
+        ExRes     <= "011";
+        ALUSrc    <= '1';
     elsif (first5 = "00110" and last2 = "10") then          -- SRL
+        TRegType  <= 'Z';
+        RegWrEn   <= '1';
+        MemWr     <= '0';
+        MemRd     <= '0';
+        WBSrc     <= '1';
+        JumpType  <= (others => 'Z');
+        ALUOp     <= "0110";
+        RegSrcA   <= '0' & ry;
+        RegSrcB   <= (others => 'Z');
+        RegDst    <= '0' & rx;
+        ExRes     <= "011";
+        ALUSrc    <= '1';
 
     -- 分支跳转
     elsif (first5 = "00010") then                           -- B
+        RegWrEn <= '0';
+        MemWr <= '0';
+        MemRd <= '0';
+        JumpType <= "000";
+        ALUOp <= "0000";
+
     elsif (first5 = "00100") then                           -- BEQZ
+        RegWrEn <= '0';
+        MemWr <= '0';
+        MemRd <= '0';
+        JumpType <= "001";
+        ALUOp <= "0000";
+        RegSrcA <= '0' & Inst(10 downto 8);
+
     elsif (first5 = "00101") then                           -- BNEZ
+        RegWrEn <= '0';
+        MemWr <= '0';
+        MemRd <= '0';
+        JumpType <= "010";
+        ALUOp <= "0000";
+        RegSrcA <= '0' & Inst(10 downto 8);
+
     elsif (first8 = "01100000") then                        -- BTEQZ
+        RegWrEn <= '0';
+        MemWr <= '0';
+        MemRd <= '0';
+        JumpType <= "011";
+        ALUOp <= "0000";
+        RegSrcA <= "1000"; -- T
+
     elsif (first5 = "11101" and last8 = "00000000") then    -- JR
+        RegWrEn <= '0';
+        MemWr <= '0';
+        MemRd <= '0';
+        JumpType <= "100";
+        ALUOp <= "0000";
+        RegSrcA <= '0' & Inst(10 downto 8);
+
     elsif (first5 = "11101" and last8 = "11000000") then    -- JALR
+        RegWrEn <= '1';
+        MemWr <= '0';
+        MemRd <= '0';
+        JumpType <= "101";
+        ALUOp <= "0000";
+        RegSrcA <= '0' & Inst(10 downto 8);
+        RegDst <= "1011"; -- RA
+        ExRes <= "010"; -- RPC
+
     elsif (Inst = "1110100000100000") then                  -- JRRA
+        RegWrEn <= '0';
+        MemWr <= '0';
+        MemRd <= '0';
+        JumpType <= "110";
+        ALUOp <= "0000";
+        RegSrcA <= "1011"; -- RA
 
     -- 比较
     elsif (first5 = "11101" and last5 = "01010") then       -- CMP
+        TRegType  <= '0';
+        RegWrEn   <= '1';
+        MemWr     <= '0';
+        MemRd     <= '0';
+        WBSrc     <= '1';
+        JumpType  <= (others => 'Z');
+        ALUOp     <= "0010";
+        RegSrcA   <= '0' & rx;
+        RegSrcB   <= '0' & ry;
+        RegDst    <= "1000";
+        ExRes     <= "000";
+        ALUSrc    <= '0';
     elsif (first5 = "11101" and last5 = "00010") then       -- SLT
+        TRegType  <= '1';
+        RegWrEn   <= '1';
+        MemWr     <= '0';
+        MemRd     <= '0';
+        WBSrc     <= '1';
+        JumpType  <= (others => 'Z');
+        ALUOp     <= "0010";
+        RegSrcA   <= '0' & rx;
+        RegSrcB   <= '0' & ry;
+        RegDst    <= "1000";
+        ExRes     <= "000";
+        ALUSrc    <= '0';
 
     -- 特殊寄存器取/赋值
     elsif (first5 = "11110" and last8 = "00000000") then    -- MFIH
+        TRegType  <= 'Z';
+        RegWrEn   <= '1';
+        MemWr     <= '0';
+        MemRd     <= '0';
+        WBSrc     <= '1';
+        JumpType  <= (others => 'Z');
+        ALUOp     <= "0000";
+        RegSrcA   <= "1001";
+        RegSrcB   <= (others => 'Z');
+        RegDst    <= '0' & rx;
+        ExRes     <= "100";
+        ALUSrc    <= 'Z';
     elsif (first5 = "11101" and last8 = "01000000") then    -- MFPC
+        TRegType  <= 'Z';
+        RegWrEn   <= '1';
+        MemWr     <= '0';
+        MemRd     <= '0';
+        WBSrc     <= '1';
+        JumpType  <= (others => 'Z');
+        ALUOp     <= "0000";
+        RegSrcA   <= (others => 'Z');
+        RegSrcB   <= (others => 'Z');
+        RegDst    <= '0' & rx;
+        ExRes     <= "001";
+        ALUSrc    <= 'Z';
     elsif (first5 = "11110" and last8 = "00000001") then    -- MTIH
+        TRegType  <= 'Z';
+        RegWrEn   <= '1';
+        MemWr     <= '0';
+        MemRd     <= '0';
+        WBSrc     <= '1';
+        JumpType  <= (others => 'Z');
+        ALUOp     <= "0000";
+        RegSrcA   <= '0' & rx;
+        RegSrcB   <= (others => 'Z');
+        RegDst    <= "1001";
+        ExRes     <= "100";
+        ALUSrc    <= 'Z';
     elsif (first8 = "01100100" and last5 = "00000") then    -- MTSP
+        TRegType  <= 'Z';
+        RegWrEn   <= '1';
+        MemWr     <= '0';
+        MemRd     <= '0';
+        WBSrc     <= '1';
+        JumpType  <= (others => 'Z');
+        ALUOp     <= "0000";
+        RegSrcA   <= '0' & rx;
+        RegSrcB   <= (others => 'Z');
+        RegDst    <= "1010";
+        ExRes     <= "100";
+        ALUSrc    <= 'Z';
 
     -- 访存
     elsif (first5 = "01101") then                           -- LI
+        TRegType  <= 'Z';
+        RegWrEn   <= '1';
+        MemWr     <= '0';
+        MemRd     <= '0';
+        WBSrc     <= '1';
+        JumpType  <= (others => 'Z');
+        ALUOp     <= "0000";
+        RegSrcA   <= (others => 'Z');
+        RegSrcB   <= (others => 'Z');
+        RegDst    <= '0' & rx;
+        ExRes     <= "101";
+        ALUSrc    <= '1';
     elsif (first5 = "10011") then                           -- LW
+        TRegType  <= 'Z';
+        RegWrEn   <= '1';
+        MemWr     <= '0';
+        MemRd     <= '1';
+        WBSrc     <= '0';
+        JumpType  <= (others => 'Z');
+        ALUOp     <= "0001";
+        RegSrcA   <= '0' & rx;
+        RegSrcB   <= (others => 'Z');
+        RegDst    <= '0' & ry;
+        ExRes     <= "011";
+        ALUSrc    <= '1';
     elsif (first5 = "10010") then                           -- LW_SP
+        TRegType  <= 'Z';
+        RegWrEn   <= '1';
+        MemWr     <= '0';
+        MemRd     <= '1';
+        WBSrc     <= '0';
+        JumpType  <= (others => 'Z');
+        ALUOp     <= "0001";
+        RegSrcA   <= "1010";
+        RegSrcB   <= (others => 'Z');
+        RegDst    <= '0' & rx;
+        ExRes     <= "011";
+        ALUSrc    <= '1';
     elsif (first5 = "11011") then                           -- SW
+        TRegType  <= 'Z';
+        RegWrEn   <= '0';
+        MemWr     <= '1';
+        MemRd     <= '0';
+        WBSrc     <= 'Z';
+        JumpType  <= (others => 'Z');
+        ALUOp     <= "0001";
+        RegSrcA   <= '0' & rx;
+        RegSrcB   <= '0' & ry;
+        RegDst    <= (others => 'Z');
+        ExRes     <= "011";
+        ALUSrc    <= '1';
     elsif (first8 = "01100010") then                        -- SW_RS
+        TRegType  <= 'Z';
+        RegWrEn   <= '0';
+        MemWr     <= '1';
+        MemRd     <= '0';
+        WBSrc     <= 'Z';
+        JumpType  <= (others => 'Z');
+        ALUOp     <= "0001";
+        RegSrcA   <= "1010";
+        RegSrcB   <= "1011";
+        RegDst   <= (others => 'Z');
+        ExRes     <= "011";
+        ALUSrc    <= '1';
     elsif (first5 = "11010") then                           -- SW_SP
+        TRegType  <= 'Z';
+        RegWrEn   <= '0';
+        MemWr     <= '1';
+        MemRd     <= '0';
+        WBSrc     <= 'Z';
+        JumpType  <= (others => 'Z');
+        ALUOp     <= "0001";
+        RegSrcA   <= "1010";
+        RegSrcB   <= '0' & rx;
+        RegDst    <= (others => 'Z');
+        ExRes     <= "011";
+        ALUSrc    <= '1';
 
     -- 空
     elsif (Inst = "0000100000000000") then                  -- NOP
+        TRegType  <= 'Z';
+        RegWrEn   <= '1';
+        MemWr     <= '0';
+        MemRd     <= '0';
+        WBSrc     <= '1';
+        JumpType  <= (others => 'Z');
+        ALUOp     <= "0101";
+        RegSrcA   <= "1100";
+        RegSrcB   <= (others => 'Z');
+        RegDst    <= "1100";
+        ExRes     <= "011";
+        ALUSrc    <= '1';
     end if;
 
 end Behavioral;
